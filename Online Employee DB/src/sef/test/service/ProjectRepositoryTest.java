4aac8b721289e4b25c8ced6b54ae55c06564cd79
haris99 <haraldsdombrovskis3@inbox.lv>
2015-07-22 13:00:30 +0300

    Added project, skill tests

--- Online Employee DB/src/sef/test/service/ProjectRepositoryTest.java
+++ Online Employee DB/src/sef/test/service/ProjectRepositoryTest.java
@@ -1,21 +1,48 @@
 package sef.test.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.TestCase;
+import sef.domain.Project;
+import sef.domain.ProjectRole;
+import sef.impl.repository.StubProjectRepositoryImpl;
+import sef.impl.repository.StubSkillRepositoryImpl;
+
+
 
 public class ProjectRepositoryTest extends TestCase{
+	StubProjectRepositoryImpl repo;
 	
-	ProjectRepositoryTest runtest = new ProjectRepositoryTest ();
+	protected void setUp(){
+		repo = new StubProjectRepositoryImpl(DataSource dataSource);
+	}
+	
+	//ProjectRepositoryTest runtest = new ProjectRepositoryTest ();
 	
 	public void testListAllProjects () {
-		runtest.testListAllProjects();
+		//runtest.testListAllProjects();
+		List<Project> projectList = new ArrayList<Project>();
+		projectList = repo.listAllProjects();
+		assertTrue(projectList.size()>=1);
 	}
 	public void testGetEmployeeProjects () {
-		runtest.testGetEmployeeProjects();
+		//runtest.testGetEmployeeProjects();
+		List<Project> projectList = new ArrayList<Project>();
+		projectList = repo.getEmployeeProjects(1);
+		assertTrue(projectList.size()>=1);
 	}
 	public void testGetEmployeeProjectRoles () {
-		runtest.testGetEmployeeProjectRoles();
+		List<ProjectRole> projectRoleList = new ArrayList<ProjectRole>();
+		projectRoleList = repo.getEmployeeProjectRoles(1, 1);
+		assertTrue(projectRoleList.size()>=1);
+		projectRoleList = repo.getEmployeeProjectRoles(999, 1);
+		assertTrue(projectRoleList.size()==0);
+		projectRoleList = repo.getEmployeeProjectRoles(1, 999);
+		assertTrue(projectRoleList.size()==0);
+		//runtest.testGetEmployeeProjectRoles();
 	}
 	public void testGetEmployeeProjectHistory () {
-		runtest.testGetEmployeeProjectHistory();
+		//runtest.testGetEmployeeProjectHistory();
 	}
 }
